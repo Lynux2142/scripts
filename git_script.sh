891510d09452c6ef7017b917fa1f6469b3a8cb3a
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-03-08 11:26:52 +0100

    suppression des retour a la ligne entre les ligne de code

--- git_script.sh
+++ git_script.sh
@@ -1,45 +1,33 @@
 #!/bin/sh
 
 RETRY="y"
-
 while [[ -n $RETRY ]]
 do
 	git status
-
 	echo
 	read -p 'files to add ? (press enter to skip) ' CHOIX
-
 	if [[ -n $CHOIX ]]
 	then
 		git add $CHOIX
 	fi
-
 	git status
-
 	echo
 	read -p 'files to remove ? (press enter to skip) ' CHOIX
-
 	if [[ -n $CHOIX ]]
 	then
 		git reset $CHOIX
 	fi
-
 	echo
 	read -p 'do you want to retry ? (press enter for no, write something for yes) ' RETRY
 done
-
 git status
-
 echo
 read -p 'commit message : ' CHOIX
-
 while [[ -z $CHOIX ]]
 do
 	read -p 'please write something : ' CHOIX
 done
-
 git commit -m "$CHOIX"
-
 echo
 read -p 'push branch : (press enter to master) ' CHOIX
 if [[ -n $CHOIX ]]
